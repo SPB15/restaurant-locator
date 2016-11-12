f95b2fb80d164d526d3a6c45f8ddd1f3c9fbefe8
Kausthub Raj Jadhav <kjadhav@uci.edu>
2016-11-12 14:37:29 -0800

    Add random function

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
-var locations = []
+var locations = [];
+var random = 0;
+
 
 $.getJSON('https://crossorigin.me/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=33.640,-117.844&rankby=distance&types=food&key=AIzaSyDxXA4Pw4SSLK5JVPBnYLBNzgTQucxXICI',function(data) {
             
@@ -13,12 +15,23 @@ $.getJSON('https://crossorigin.me/https://maps.googleapis.com/maps/api/place/nea
                            });
         }
     
-    
 for(var i = 0; i<4;i++)
         {
     console.log('THE FUCKING DATA : ', locations[i].name, ' -- ', locations[i].lat, ' -- ', locations[i].lng, ' -- ' );
     
         }
+
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+random_index = Math.floor((Math.random() * 10) + 1);
+console.log('THE FUCKING RANDOM NUMBER IS ======', random_index);
     
-    $('.results').html(String(locations[0].name));
+$('.results').html(String(locations[random_index].name));
+    
+    
+    
+    
+    
 });
